ea65dac3df40a85e70b8b5c8c5aae605aa5568a2
EC2 Default User <ec2-user@ip-172-31-29-19.us-east-2.compute.internal>
2020-03-16 22:05:53 +0000

    fix bug

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,9 +25,9 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category = Category.find(params[:id])
+    @category = params[:id] ? Category.find(params[:id]) : Category.new
     @category.attributes = params[:category]
-    if request.post?
+    if request.post? and @category
       respond_to do |format|
         format.html { save_category }
         format.js do 
